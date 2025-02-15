dd15f6742611aad2ec718f439f24a6c015c456aa
temmu742 <temmu1@mail.com>
2025-02-15 22:25:08 +0200

    Update CatDto.cs

--- Models/CatDto.cs
+++ Models/CatDto.cs
@@ -1,13 +1,1 @@
-﻿namespace CatAPI.Models
-{
-    public class CatDto
-    {
-        public int Id { get; set; }
-        public required string CatId { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public required byte[] Image { get; set; }
-        public DateTime Created { get; set; }
-        public List<string> Tags { get; set; } = new List<string>();
-    }
-}
+
